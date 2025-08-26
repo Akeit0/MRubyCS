5cabfb6bfa77e37e028432309a59d1e7ab53ca0b
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-08-26 22:42:11 +0900

    rename MRubyValueUnsafeProxy to MRubyValueUnsafeAlias and add new unsafe alias structs

--- src/MRubyCS/Internals/MRubyValueUnsafeAlias.cs
+++ src/MRubyCS/Internals/MRubyValueUnsafeAlias.cs
@@ -1,13 +1,26 @@
 ﻿using System.Runtime.InteropServices;
 
 namespace MRubyCS.Internals;
+
 [StructLayout(LayoutKind.Explicit)]
 internal struct MRubyValueImmediateUnsafeAlias
 {
     [FieldOffset(0)]
     public nuint Type;
+
     [FieldOffset(8)]
     public long Bits;
+
     [FieldOffset(8)]
     public double Float;
+}
+
+[StructLayout(LayoutKind.Explicit)]
+internal struct MRubyValueObjectUnsafeAlias
+{
+    [FieldOffset(0)]
+    public object Object;
+
+    [FieldOffset(8)]
+    public nuint Type;
 }
